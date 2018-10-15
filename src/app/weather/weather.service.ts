f8d34d3c7b889a40e04f88fc73f343eea881fec3
Austin Dragone <austindrag1@gmail.com>
2018-10-15 14:13:50 -0400

    Weather for user

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+weather: String = "The weather in your area, for today, is ";
+userWeather: String = "";
+userLocation: String = "";
+
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
   constructor() { }
+}
+
+getUserWeather( location ) {
+  userWeather = getWeather(userLocation);
+  return weather + userWeather;
 }
